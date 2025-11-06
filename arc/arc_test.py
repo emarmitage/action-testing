d956a989c82ec6b394d18a6225061b5805ee5298
Emma Armitage <emma.armitage@gov.bc.ca>
2025-11-05 16:03:35 -0800

    uncomment out code

--- arc/arc_test.py
+++ arc/arc_test.py
@@ -6,18 +6,16 @@ from arcgis.gis import GIS
 import geopandas as gpd
 
 
-# username = os.getenv('AGO_USERNAME')
-# password = os.getenv('AGO_PASSWORD')
-# portal = os.getenv('AGO_PORTAL')
+username = os.getenv('AGO_USERNAME')
+password = os.getenv('AGO_PASSWORD')
+portal = os.getenv('AGO_PORTAL')
 oid = os.getenv('OID_POWER_AUTOMATE')
 
-print(oid)
+gis = GIS(url=portal, username=username, password=password)
 
-# gis = GIS(url=portal, username=username, password=password)
+print("Logged in as: " + username)
 
-# print("Logged in as: " + username)
+search_results = gis.content.search(query="type:Feature Layer", max_items=10)
 
-# search_results = gis.content.search(query="type:Feature Layer", max_items=10)
-
-# for item in search_results:
-#     print(f"Title: {item.title}, Type: {item.type}, ID: {item.id}") 
+for item in search_results:
+    print(f"Title: {item.title}, Type: {item.type}, ID: {item.id}") 
